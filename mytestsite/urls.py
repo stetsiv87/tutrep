76d265b7d59d6b832130de184dad8fc2de089786
stetsiv87 <stetsiv.y@gmail.com>
2016-04-30 02:23:03 +0300

    security/registration

--- mytestsite/urls.py
+++ mytestsite/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'', include('myblog.urls')),
-
-]
+)
